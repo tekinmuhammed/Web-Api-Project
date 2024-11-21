c3e7db13d39b73f88b5c1f0dd6d0936e9fc89e60
Muhammed Tekin <muhammedtekin@piton.com.tr>
2024-11-21 22:14:44 +0300

    Resolving merge conflicts while merging main into development

--- obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed8ab054ad838372c0745449093fb00c754882df")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9723038407d06e2832aad3f78522877026b7c16")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
